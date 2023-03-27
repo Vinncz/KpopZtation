263035ac426bf417b88c105c7dac21d9e7db52d0
Vin <kevgun03@gmail.com>
2023-03-27 19:46:26 +0700

    init commit

---
+++ Kel3_KpopZtation/Queries/Inserting Into The Database.sql
@@ -0,0 +1,48 @@
+﻿INSERT INTO Customer (CustomerID, CustomerName, CustomerEmail, CustomerPassword, CustomerGender, CustomerAddress, CustomerRole)
+VALUES
+    (1, 'John Doe', 'johndoe@email.com', 'password123', 'Male', '123 Main Street', 'User'),
+    (2, 'Jane Smith', 'janesmith@email.com', 'password456', 'Female', '456 Elm Street', 'User'),
+    (3, 'Bob Johnson', 'bobjohnson@email.com', 'password789', 'Male', '789 Oak Street', 'Admin'),
+    (4, 'Alice Brown', 'alicebrown@email.com', 'password321', 'Female', '321 Pine Street', 'User'),
+    (5, 'Charlie Davis', 'charliedavis@email.com', 'password654', 'Male', '654 Maple Street', 'User');
+
+INSERT INTO Artist (ArtistID, ArtistName, ArtistImage)
+VALUES
+    (1, 'The Beatles', 'the-beatles.jpg'),
+    (2, 'Elvis Presley', 'elvis-presley.jpg'),
+    (3, 'Michael Jackson', 'michael-jackson.jpg'),
+    (4, 'Madonna', 'madonna.jpg'),
+    (5, 'Led Zeppelin', 'led-zeppelin.jpg');
+
+INSERT INTO Album (AlbumID, ArtistID, AlbumName, AlbumImage, AlbumPrice, AlbumStock, AlbumDescription)
+VALUES
+    (1, 1, 'Abbey Road', '/images/abbey-road.jpg', 20, 1000, 'The Beatles eleventh studio album'),
+    (2, 2, 'Elvis Presley', '/images/elvis-presley-album.jpg', 15, 500, 'Elvis Presleys debut studio album'),
+    (3, 3, 'Thriller', '/images/thriller.jpg', 25, 2000, 'Michael Jacksons sixth studio album'),
+    (4, 4, 'Like a Virgin', '/images/like-a-virgin.jpg', 18, 1500, 'Madonnas second studio album'),
+    (5, 5, 'Led Zeppelin IV', '/images/led-zeppelin-iv.jpg', 22, 1200, 'Led Zeppelins fourth studio album');
+
+INSERT INTO Cart (CustomerID , AlbumID , Quantity )
+VALUES
+    (1, 1, 2),
+    (1, 3, 1),
+    (2, 2, 3),
+    (3, 5, 4),
+    (4, 4, 2);
+
+INSERT INTO TransactionHeader (TransactionID , TransactionDate , CustomerID )
+VALUES
+    (1, '2023-03-27', 1),
+    (2, '2023-03-28', 2),
+    (3, '2023-03-29', 3),
+    (4, '2023-03-30', 4),
+    (5, '2023-03-31', 5);
+
+INSERT INTO TransactionDetail (TransactionID , AlbumID , Quantity )
+VALUES
+    (1, 1, 2),
+    (1, 3, 1),
+    (2, 2, 3),
+    (3, 5, 4),
+    (4, 4, 2),
+    (5, 1, 1);
---
+++ Kel3_KpopZtation/Queries/Making The Database.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE Customer (
+    CustomerID INT PRIMARY KEY NOT NULL,
+    CustomerName VARCHAR(50) NOT NULL,
+    CustomerEmail VARCHAR(50) NOT NULL,
+    CustomerPassword VARCHAR(50) NOT NULL,
+    CustomerGender VARCHAR(6) NOT NULL,
+    CustomerAddress VARCHAR(100) NOT NULL,
+    CustomerRole VARCHAR(5) NOT NULL
+);
+
+CREATE TABLE Artist (
+    ArtistID INT PRIMARY KEY NOT NULL,
+    ArtistName VARCHAR(50) NOT NULL,
+    ArtistImage VARCHAR(50)
+);
+
+CREATE TABLE Album (
+    AlbumID INT PRIMARY KEY NOT NULL,
+    ArtistID INT FOREIGN KEY REFERENCES Artist(ArtistID) NOT NULL,
+    AlbumName VARCHAR(50) NOT NULL,
+    AlbumImage VARCHAR(50),
+    AlbumPrice INT NOT NULL,
+    AlbumStock INT NOT NULL,
+    AlbumDescription VARCHAR(255)
+);
+
+CREATE TABLE Cart (
+    CustomerID INT FOREIGN KEY REFERENCES Customer(CustomerID) NOT NULL,
+    AlbumID INT FOREIGN KEY REFERENCES Album(AlbumID) NOT NULL,
+    Quantity INT NOT NULL
+);
+
+CREATE TABLE TransactionHeader (
+    TransactionID INT PRIMARY KEY NOT NULL,
+    TransactionDate DATE NOT NULL,
+    CustomerID INT FOREIGN KEY REFERENCES Customer(CustomerID) NOT NULL
+);
+
+CREATE TABLE TransactionDetail (
+    TransactionID INT FOREIGN KEY REFERENCES TransactionHeader(TransactionID) NOT NULL,
+    AlbumID INT FOREIGN KEY REFERENCES Album(AlbumID) NOT NULL,
+    Quantity INT NOT NULL
+);
