2a559e1d478ab8716183a4dcda44aa9c795a48a0
Vin <kevgun03@gmail.com>
2023-04-19 13:37:58 +0700

    Refactored the customer's field validation to the CustomerController, from originally the AuthController.
    New generation of CustomerRepo methods are waiting to be implemented.

--- Kel3_KpopZtation/Controllers/AuthController.cs
+++ Kel3_KpopZtation/Controllers/AuthController.cs
@@ -22,69 +22,48 @@ namespace Kel3_KpopZtation.Controllers {
             Customer AssociatedAccount = null;
             List<string> ErrorMsgs = new List<string>();
 
-            /* Email validation has been delegated to appropriate method */
-            var EmailValidationResult = ValidateEmail(Email); ErrorMsgs.Add(EmailValidationResult.ErrorMsg);
-            var EmailStatus = EmailExistOnDatabase(Email, ""); ErrorMsgs.Add(EmailStatus.ErrorMsg);
+            bool EmailValidationResult = CustomerController.ValidateEmail(Email, ErrorMsgs); 
+            bool PasswordValidationResult = CustomerController.ValidatePassword(Password, ErrorMsgs); 
 
-            /* Password validation has been delegated to appropriate method */
-            var PasswordValidationResult = ValidatePassword(Password); ErrorMsgs.Add(PasswordValidationResult.ErrorMsg);
+            var EmailExistence = EmailExistOnDatabase(Email, ""); 
+            ErrorMsgs.Add(EmailExistence.ErrorMsg);
 
-            /* Removing empty error msgs */
             FormatController.RemoveEmptyString(ErrorMsgs);
 
-            /* Determining whether the given parameter can be checked further */
-            bool ParameterIsValid = EmailValidationResult.isValid  && PasswordValidationResult.isValid && EmailStatus.doesExist;
+            bool ParameterIsValid = EmailValidationResult  && PasswordValidationResult && EmailExistence.doesExist;
             if ( ParameterIsValid ) {
-
-                /* Determining whether there is any account associated with said email and password */
                 AssociatedAccount = CustomerRepo.EmailPasswordMatch(Email, Password);
 
-                /* When there is */
                 if (AssociatedAccount != null) { 
-                    
-                    /* Store client's login info on a Session that self-expire after the connection is terminated */
                     CookieController.AssignSession(AssociatedAccount);
 
-                    /* If client opt to auto login, then assign a cookie to client */
-                    if (SetCookie) CookieController.AssignAuthCookie();
+                    if (SetCookie) 
+                        CookieController.AssignAuthCookie();
                 }
             }
-            
-            /* Return the AssociatedAccount (if any), and the error messages found along the way */
+
             return (AssociatedAccount, ErrorMsgs);
         }
-
-        public static (Customer CreatedAccount, List<string> ErrorMsgs) Register 
-            (string name, string email, string sex, string address, string password) {
+        public static (Customer CreatedAccount, List<string> ErrorMsgs) Register (string Name, string Email, string Sex, string Address, string Password) {
 
             Customer CreatedAccount = null;
             List<string> ErrorMsgs = new List<string>();
 
-            var NameValidationResult = ValidateName(name); ErrorMsgs.Add(NameValidationResult.ErrorMsg);
+            bool NameValidationResult = CustomerController.ValidateName(Name, ErrorMsgs);
+            bool EmailValidationResult = CustomerController.ValidateEmail(Email, ErrorMsgs);
 
-            var EmailValidationResult = ValidateEmail(email); ErrorMsgs.Add(EmailValidationResult.ErrorMsg);
-            var EmailExistenceInDB = EmailExistOnDatabase(email, "There are already an account ascosiated with that email! Try logging in.");  ErrorMsgs.Add(EmailExistenceInDB.ErrorMsg);
+            var EmailExistenceInDB = EmailExistOnDatabase(Email, "There are already an account ascosiated with that email! Try logging in.");  
+                ErrorMsgs.Add(EmailExistenceInDB.ErrorMsg);
 
-            var SexValidationResult = ValidateSex(sex); ErrorMsgs.Add(SexValidationResult.ErrorMsg);
-
-            var AddressValidationResult = ValidateAddress(address); ErrorMsgs.Add(AddressValidationResult.ErrorMsg);
-
-            var PasswordValidationResult = ValidatePassword(password); ErrorMsgs.Add(PasswordValidationResult.ErrorMsg);
+            bool SexValidationResult = CustomerController.ValidateSex(Sex, ErrorMsgs);
+            bool AddressValidationResult = CustomerController.ValidateAddress(Address, ErrorMsgs);
+            bool PasswordValidationResult = CustomerController.ValidatePassword(Password, ErrorMsgs);
 
             FormatController.RemoveEmptyString(ErrorMsgs);
 
-            bool ParameterIsValid = NameValidationResult.isValid && EmailValidationResult.isValid && EmailExistenceInDB.doesExist == false
-                                    && SexValidationResult.isValid && AddressValidationResult.isValid && PasswordValidationResult.isValid;
-            
-            System.Diagnostics.Debug.WriteLine(NameValidationResult.isValid ? "name valid" : "name invalid");
-            System.Diagnostics.Debug.WriteLine(EmailValidationResult.isValid ? "email valid" : "email invalid");
-            System.Diagnostics.Debug.WriteLine(EmailExistenceInDB.doesExist ? "email exists" : "email does not exist");
-            System.Diagnostics.Debug.WriteLine(SexValidationResult.isValid ? "Sex valid" : "Sex invalid");
-            System.Diagnostics.Debug.WriteLine(AddressValidationResult.isValid ? "Address valid" : "Address invalid");
-            System.Diagnostics.Debug.WriteLine(PasswordValidationResult.isValid ? "Password valid" : "Password invalid");
-
-            if (ParameterIsValid) {
-                CreatedAccount = CustomerHandler.MakeCustomer(name, email, sex, address, password, "Buyer");
+            bool ParameterIsValid = NameValidationResult && EmailValidationResult && EmailExistenceInDB.doesExist == false && SexValidationResult && AddressValidationResult && PasswordValidationResult;
+            if ( ParameterIsValid ) {
+                CreatedAccount = CustomerHandler.MakeCustomer(Name, Email, Sex, Address, Password, "Buyer");
                 CustomerHandler.InsertCustomer(CreatedAccount);
 
                 if (CreatedAccount != null) {
@@ -94,11 +73,15 @@ namespace Kel3_KpopZtation.Controllers {
 
             return (CreatedAccount, ErrorMsgs);
         }
+        public static (bool doesExist, string ErrorMsg) EmailExistOnDatabase (string email, string customErrorMsg) {
+            if (CustomerRepo.ExistByEmail(email) != null)
+                return (true, customErrorMsg);
 
-        /* 
-         * If there is an Authentication Cookie stored in the client's browser,
-         * then make the session as per Cookie's data dictate. 
-         */
+            if (FormatController.NullWhitespacesOrEmpty(customErrorMsg) || FormatController.TrimLen(customErrorMsg) <= 0)
+                return (false, "There is no account associated with that email!");
+            else
+                return (false, customErrorMsg);
+        }
         public static void MakeSessionFromCookie () {
             HttpCookie AuthCookie = HttpContext.Current.Request.Cookies["AuthInfo"];
 
@@ -110,104 +93,9 @@ namespace Kel3_KpopZtation.Controllers {
                 HttpContext.Current.Session["AuthInfo"] = CustomerRepo.ExistByID( Convert.ToInt32(AuthCookie.Values["CustomerID"]) );
             } 
         }
-
         public static Customer ExtractCustomer () {
             return (Customer) HttpContext.Current.Session["AuthInfo"];
         }
-
-        public static (bool isValid, string ErrorMsg) ValidateName (string name) {
-            
-            /* Cek apakah string parameter bisa diproses */
-            if ( FormatController.NullWhitespacesOrEmpty(name) ) {
-                return (false, "Name cannot be empty or all whitespaces!");
-
-            /* Cek apakah panjang string parameter berada diantara 5-50 karakter */
-            } else if ( FormatController.TrimLen(name) < 5 || FormatController.TrimLen(name) > 50 ) {
-                return (false, "That is such a long name! Try using aliases.");
-            
-            }
-
-            return (true, "");
-        }
-
-        public static (bool isValid, string ErrorMsg) ValidateEmail (string email) {
-
-            /* Cek apakah string parameter bisa diproses */
-            if ( FormatController.NullWhitespacesOrEmpty(email) ) {
-                return (false, "Email cannot be empty or all whitespaces!");
-
-            /* Cek apakah itu berformat email */
-            } else if ( !FormatController.InEmailFormat(email) ) {
-                return (false, "Email is not in a correct format!");
-
-            } 
-
-            return (true, "");
-        }
-
-        public static (bool isValid, string ErrorMsg) ValidatePassword (string password) {
-
-            if ( FormatController.NullWhitespacesOrEmpty(password) ) {
-                return (false, "Password cannot be empty!");
-
-            } else if ( !FormatController.InAlphaNumericFormat(password) ) {
-                return (false, "Password must be an alphanumeric!");
-            }
-            
-            if ( FormatController.TrimLen(password) > 50 ) {
-                return (false, "Password is too long! Try keeping it under 50 characters.");
-            } 
-
-            return (true, "");
-        }
-
-        public static (bool isValid, string ErrorMsg) ValidateSex (string sex) {
-            System.Diagnostics.Debug.WriteLine(sex);
-            /* Cek apakah variabelnya bisa diproses */
-            if (FormatController.NullWhitespacesOrEmpty(sex) || FormatController.TrimLen(sex) < 4) {
-                return (false, "Gender must be picked!");
-
-            /* Cek apakah memenuhi salah satu dari dua kemungkinan */
-            } else if ( !(sex != "Male" || sex != "Female") ) {
-                return (false, "Gender must be either Male or Female!");
-            }
-
-            if ( sex != "Male" || sex != "Female" )
-                return (true, "");
-            else
-                return (false, "Something went wrong.");
-        }
-
-        public static (bool isValid, string ErrorMsg) ValidateAddress (string address) {
-
-            /* Cek apakah string parameter bisa diproses */
-            if ( FormatController.NullWhitespacesOrEmpty(address) ) {
-                return (false, "Address cannot be empty or all whitespaces!");
-
-            /* Cek apakah itu berakhiran dengan "Street" */
-            } else if ( !FormatController.EndsWith(address, "Street") ) {
-                return (false, "An address must end with \"Street\"!");
-
-            } 
-            
-            if ( FormatController.TrimLen(address) > 100 ) {
-                return (false, "Address is too long! Try shortening it to 100 characters max.");
-            }
-
-            return (true, "");
-        }
-
-        public static (bool doesExist, string ErrorMsg) EmailExistOnDatabase (string email, string customErrorMsg) {
-            if (CustomerRepo.ExistByEmail(email) != null)
-                return (true, customErrorMsg);
-
-            /* Jika gamau custom error msg */
-            if (FormatController.NullWhitespacesOrEmpty(customErrorMsg) || FormatController.TrimLen(customErrorMsg) <= 0)
-                return (false, "There is no account associated with that email!");
-            else
-                return (false, customErrorMsg);
-        }
-    
         public static void SignOut () {
             foreach (string SavedCookie in HttpContext.Current.Request.Cookies.AllKeys) {
                 // System.Diagnostics.Debug.WriteLine(SavedCookie);
--- Kel3_KpopZtation/Repositories/CustomerRepo.cs
+++ Kel3_KpopZtation/Repositories/CustomerRepo.cs
@@ -3,17 +3,107 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Kel3_KpopZtation.Models;
+using Kel3_KpopZtation.Controllers;
 
 namespace Kel3_KpopZtation.Repositories {
     public class CustomerRepo {
         private static KZDBEntities db = ConnectionMaster.CopyInstance();
+
+        /* Miscelaneous Operations */
+        public static int GetLatestCustomerID () {
+            /* O(log n) algorithm */
+            Customer latestCustomer = db.Customers.OrderByDescending(c => c.CustomerID).FirstOrDefault();
+            if (latestCustomer != null)
+                return latestCustomer.CustomerID;
+
+            return 0;
+        }
+        public static Customer AuthenticateCustomer (string CustomerEmail, string CustomerPassword){
+            Customer This = Find(CustomerEmail);
+
+            if (This != null && This.CustomerPassword == CustomerPassword)
+                return This;
+
+            return null;
+        }
+        public static Customer Find (string CustomerEmail) {
+            return (from Customer in db.Customers
+                    where Customer.CustomerEmail == CustomerEmail
+                    select Customer).FirstOrDefault();
+        }
+
+        /* CRUD Operations */
+        public static List<Customer> Select () {
+            return (from Customer in db.Customers select Customer).ToList();
+        }
+        public static Customer Find (int CustomerID) {
+            return (from Customer in db.Customers
+                    where Customer.CustomerID == CustomerID
+                    select Customer).FirstOrDefault();
+        }
+        public static bool Insert (Customer c) {
+            db.Customers.Add(c);
+            return Save();
+        }
+        public static bool Update (int TargetCustomerID, string NewName, string NewEmail, string NewGender, string NewAddress, string NewPassword) {
+            Customer TargetData = Find(TargetCustomerID);
+            RewriteIfChanged (NewName, NewEmail, NewGender, NewAddress, NewPassword, TargetData);
+
+            return Save();
+        }
+            private static void RewriteIfChanged (string NewName, string NewEmail, string NewGender, string NewAddress, string NewPassword, Customer TargetData) {
+                if (TargetData.CustomerName != NewName) {
+                    TargetData.CustomerName = NewName;
+                }
+
+                if (TargetData.CustomerEmail != NewEmail) {
+                    TargetData.CustomerEmail = NewEmail;
+                }
+
+                if (TargetData.CustomerAddress != NewAddress) {
+                    TargetData.CustomerAddress = NewAddress;
+                }
+
+                if (TargetData.CustomerGender != NewGender) {
+                    TargetData.CustomerGender = NewGender;
+                }
+
+                if (TargetData.CustomerPassword != NewPassword) {
+                    TargetData.CustomerPassword = NewPassword;
+                }
+            }
+        public static bool Delete (int TargetCustomerID) {
+            Customer TargetData = Find(TargetCustomerID);
+            db.Customers.Remove(TargetData);
+
+            return Save();
+        }
+        public static bool Save () {
+            try {
+                db.SaveChanges();
+                return true;
+
+            } catch (System.Data.Entity.Validation.DbEntityValidationException ex) {
+                PrintError(ex);
+                return false;
+
+            }
+        }
+            private static void PrintError (System.Data.Entity.Validation.DbEntityValidationException ex) {
+                foreach (var entityValidationErrors in ex.EntityValidationErrors) {
+                    foreach (var validationError in entityValidationErrors.ValidationErrors) {
+                        System.Diagnostics.Debug.WriteLine("Property: " + validationError.PropertyName + " Error: " + validationError.ErrorMessage);
+                    }
+                }
+            }
+
+    //  OLD METHOD ________________________________________________________________
         public static void InsertCustomer (Customer c) {
             try {
                 db.Customers.Add(c);
                 db.SaveChanges();
             } catch (System.Data.Entity.Validation.DbEntityValidationException ex) {
-                foreach (var entityValidationErrors in ex.EntityValidationErrors)
-                {
+                foreach (var entityValidationErrors in ex.EntityValidationErrors) {
                     foreach (var validationError in entityValidationErrors.ValidationErrors)
                     {
                         System.Diagnostics.Debug.WriteLine("Property: " + validationError.PropertyName + " Error: " + validationError.ErrorMessage);
@@ -39,11 +129,6 @@ namespace Kel3_KpopZtation.Repositories {
         public static List<Customer> Retrieve () {
             return (from Customer in db.Customers
                     select Customer).ToList();
-        }
-        public static int GetLatestCustomerID () {
-            return (from Customer in db.Customers 
-                    orderby Customer.CustomerID descending 
-                    select Customer.CustomerID).FirstOrDefault();
         }
     }
 }
